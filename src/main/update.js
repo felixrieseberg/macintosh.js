8f7365e895848d0c356d9533570bebc23257d5f5
Felix Rieseberg <felix@felixrieseberg.com>
2020-07-28 16:54:36 -0700

    fix: Lint forever

--- src/main/update.js
+++ src/main/update.js
@@ -1,7 +1,7 @@
 const { app } = require("electron");
 
 function setupUpdates() {
-  if (app.isPackaged && process.platform !== 'linux') {
+  if (app.isPackaged && process.platform !== "linux") {
     require("update-electron-app")({
       repo: "felixrieseberg/macintosh.js",
       updateInterval: "1 hour",
